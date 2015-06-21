67e7f425183c1ee8a0cc1fb5d25cf5a69bb27baa
hoey <nznial@gmail.com>
2015-06-21 22:10:34 +0900

    removed bootstrap antlr : use homebrew

--- bootstrap_antlr.sh
+++
@@ -1,5 +0,0 @@
-cd /usr/local/lib
-sudo curl -O http://www.antlr.org/download/antlr-4.5-complete.jar
-export CLASSPATH=".:/usr/local/lib/antlr-4.5-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.5-complete.jar'
-alias grun='java org.antlr.v4.runtime.misc.TestRig'
