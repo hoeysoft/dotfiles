a4a8afd425e12d28cc520a1df161a40166a88c2e
hoey <nznial@gmail.com>
2015-07-15 11:20:51 +0900

    install_windwos_gvim.bat added

---
+++ install_windows_gvim.bat
@@ -0,0 +1,12 @@
+rem Run as admin
+
+@echo off
+pushd %~dp0
+call mklink /d %HOMEDRIVE%%HOMEPATH%\.dotfiles %CD%
+call mklink /d %HOMEDRIVE%%HOMEPATH%\.vim %CD%\vim
+call mklink /d %HOMEDRIVE%%HOMEPATH%\vimfiles %CD%\vim
+call mklink %HOMEDRIVE%%HOMEPATH%\_vimrc %CD%\vimrc
+call mklink %HOMEDRIVE%%HOMEPATH%\_gvimrc %CD%\gvimrc
+call git submodule update --init --recursive
+call vim -u NONE -N -S bundles.vim +PluginInstall +qall
+popd
